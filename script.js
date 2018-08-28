4e66bf142ec89f59f4e075eae27c7afd35763629
tijsv <tijs.verellen@hotmail.com>
2018-08-28 21:34:06 +0200

    content works 1/2

--- script.js
+++ script.js
@@ -66,51 +66,134 @@ function main() {
     }
   }
 
+  // [ TITLE, [ IMAGES ], TAGS, CONTENT ]
   var allWorksArray = [
     [
       "Landingpage Zenjoy Technologies",
-      "works/landingspage_large.png",
-      "front-end & logo & video"
+      ["works/landingspage.png", "works/landingspage_large.png"],
+      "front-end & logo & video",
+      `
+      <p>
+        For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.<br>
+        After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.<br>
+        Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+        To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ],
     [
       "Van Dessel Plastics",
-      "works/vandesselplastic.png",
-      "front-end"
+      ["works/vandesselplastic.png", "works/vandesselplastic_large.png"],
+      "front-end",
+      `
+      <p>
+        For the base of this website I used a template chosen by the customer, Van Dessel Plastics. This template was written in PHP, so in the end I still had to write a lot of the HTML and CSS myself. <br>Also I had to think a lot about the right data structure used in the back-end, so it would be easy for the customer to add content to the website through the CMS. Their are a lot of different products to show and it had to be done in a basic and easily understandable way.<br>
+        While trying to fit everything together, I couldn't find a good way to display the former logo of Van Dessel Plastics. So I contacted the customer and asked if they would mind me designing a present-day logo, which they agreed to. Today, Van Dessel Plastics uses both website and logo made by me.
+      </p>
+      <a href="https://www.vdplastics.be/" target="_blank">website</a>
+      `
     ],
     [
       "Waardevol Werk",
-      "works/waardevolwerk.png",
-      "front-end"
+      ["works/waardevolwerk.png", "works/waardevolwerk_large.png"],
+      "front-end",
+      `
+      <p>
+        At the end of my internship at Zenjoy I was assigned to design and build a website for VIVO. I went to VIVO in Brussels to attend a meeting where we exchanged ideas to start the project. We tackled a lot of problems concerning the way they wanted to visualize all the information on their website. The content consists of nine subjects, which had to easy to access by the user. That's why we came up with a three by three grid to show these nine subjecs on the front page of the website. We found a template that fit this idea and so I started to build the website.
+      </p>
+      <a href="https://www.waardevolwerk.be/" target="_blank">website</a>
+      `
     ],
     [
-      "cinema.me (own project & under construction)",
-      "works/cinemame.png",
-      "front-end & back-end"
+      "cinema.me (under construction)",
+      ["works/cinemame.png", "works/cinemame_large.png"],
+      "front-end & back-end",
+      `
+      <p>
+      This website is a personal project. I love to watch movies and series, but I found myself forgetting which ones I already watched. That's why I wanted to make a website where people can save the movies and series they watched, and get notified when a sequel or new season comes out. <br>
+      To realise this, it was necessary for me to learn how to build not only the front-end, but also the back-end of a website myself. I did some research on which programming language to use for this. I already worked with PHP before, but I found a lot of interesting information about Node.js. I eventually chose to go for the newer Node.js technology to build my back-end.<br>
+      So the back-end of the website is written with Node.js and Express. I used MongoDB and the Mongoose module to make a database where I store user data as well as some of the data I retrieve from TMDB, so the number of API request (handled server-side) doesn't reach its limit. To secure users data I used Passport.js. Finally for the front-end I used the EJS templating language, SASS and vanilla javascript.<br>
+      This project is still under construction, but users can already make accounts and use a few of the final features. I included this project mostly to show I understand the fundamentals of the back-end of a web application.
+      </p>
+      <a href="http://cinemame.herokuapp.com/" target="_blank">website</a>
+      `
     ],
     [
       "Subbassmentz",
-      "works/subbassmentz.png",
-      "logo & 3D"
+      ["works/subbassmentz.png", "works/subbassmentz_2.png"],
+      "logo & 3D",
+      `
+      <p>
+      For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.
+      After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.
+      Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+      To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ],
     [
       "Grietje",
-      "works/grietje.png",
-      "poster & video"
+      ["works/grietje.png", "works/grietje_2.png"],
+      "poster & video",
+      `
+      <p>
+      For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.
+      After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.
+      Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+      To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ],
     [
       "Another World",
-      "works/anotherworld.png",
-      "poster & video"
+      ["works/anotherworld.png", "works/anotherworld_2.png"],
+      "poster & video",
+      `
+      <p>
+      For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.
+      After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.
+      Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+      To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ],
     [
       "Bezorgde Berlaarse Burgers",
-      "works/bbb.png",
-      "logo"
+      ["works/bbb.png", "works/bbb_2.png"],
+      "logo",
+      `
+      <p>
+      For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.
+      After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.
+      Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+      To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ],
     [
       "Infographic of Electric Production",
-      "works/infographic.png",
-      "infographic"
+      ["works/infographic.png", "works/infographic_2.png"],
+      "infographic",
+      `
+      <p>
+      For my bachelor's thesis I built a landingpage directed towards technology companies for Zenjoy, my internship company at the time. A lot of research went into this project. I started with understanding the basic fundamentals of a landingpage, then examined the best marketing models for the job and also questioned multiple businesses about their experience with Zenjoy.
+      After I accumulated this information, I started designing a template for different kinds of landingpages that might be needed in the future, with one finished design for Zenjoy Technology. I was asked to keep a clear link between the main Zenjoy brand and the different landingpages in doing so.
+      Then I built the page from scratch, making use of Zenjoys own CMS Nimbu. All content you see on the website is made by me.
+      To top of my thesis, I also made an introduction video that brings together all information on the landingpage.
+      </p>
+      <a href="http://tech.zenjoy.be/" target="_blank">website</a>
+      <a href="https://www.youtube.com/watch?v=r6qCmm3rf9E" target="_blank">video</a>
+      `
     ]
   ];
 
@@ -120,13 +203,35 @@ function main() {
   var allWorks = document.getElementsByClassName('single-work');
   var details = document.getElementById('work-details');
   var title = details.getElementsByTagName('h4')[0];
+  var text = details.getElementsByClassName('text')[0].getElementsByClassName('content')[0];
+  var image = details.getElementsByClassName('work-img')[0];
+  var nextImage = image.getElementsByClassName('next-img')[0];
   // on click function for every single work
   for(i = 0; i < allWorks.length; i++) {
     allWorks[i].onclick = (e) => {
       // fix: i is not in scope of onclick function!!!
       var i = parseInt(e.target.dataset.index);
       title.dataset.string = allWorksArray[i][0];
-      details.getElementsByClassName('work-img')[0].style.backgroundImage = 'url( ' + allWorksArray[i][1] + ')';
+      image.style.backgroundImage = 'url( ' + allWorksArray[i][1][0] + ')';
+      text.innerHTML = allWorksArray[i][3];
+
+      // if there are multiple image for this work
+      if(allWorksArray[i][1].length > 1) {
+        image.dataset.images = allWorksArray[i][1];
+        image.style.backgroundImage = 'url( ' + allWorksArray[i][1][1] + ')';
+        image.dataset.index = 1;
+        nextImage.style.display = "block";
+        nextImage.onclick = () => {
+          var imageIndex = parseInt(image.dataset.index)+1;
+          if(imageIndex == allWorksArray[i][1].length) {
+            imageIndex = 0;
+          }
+          image.dataset.index = imageIndex;
+          image.style.backgroundImage = 'url( ' + allWorksArray[i][1][imageIndex] + ')';
+        }
+      } else {
+        nextImage.style.display = "none";
+      }
       for(j = 0; j < allWorks.length; j++) {
         allWorks[j].classList.add('grow');
       }
@@ -134,32 +239,33 @@ function main() {
         hideWorks(x = 0, allWorks, () => {
           setTimeout(() => {
             details.style.display = "block";
-            // fix for bad display lower than 980 height
-            if(767 < window.innerWidth && window.innerWidth < 980 && allWorks.length > 6) {
-              // console.log(window.innerWidth, 'all works with index higher than', 5, ' need to get display none for good display..');
-              for(i = 5; i < allWorks.length; i++) {
-                allWorks[i].style.display = "none";
-              }
-              title.scrollIntoView({block: "start", inline: "start"});
-            } else if(480 < window.innerWidth && window.innerWidth < 768 && allWorks.length > 8) {
-              // console.log(window.innerWidth, 'all works with index higher than', 7, ' need to get display none for good display..');
-              for(i = 7; i < allWorks.length; i++) {
-                allWorks[i].style.display = "none";
-              }
-              title.scrollIntoView({block: "start", inline: "start"});
-            } else if(window.innerWidth < 481 && allWorks.length > 4) {
-              // console.log(window.innerWidth, 'all works with index higher than', 3, ' need to get display none for good display..');
-              for(i = 3; i < allWorks.length; i++) {
-                allWorks[i].style.display = "none";
-              }
-              title.scrollIntoView({block: "start", inline: "start"});
-            }
           }, 400);
           setTimeout(() => {
             writeLetters(20, index = 0, title.dataset.string.split(''), title, () => {
-              details.getElementsByClassName('work-details-inner')[0].getElementsByTagName('p')[0].classList.add('visible');
-              details.getElementsByClassName('work-img')[0].classList.add('visible');
+              details.getElementsByClassName('work-details-inner')[0].getElementsByClassName('content')[0].classList.add('visible');
+              image.classList.add('visible');
               details.getElementsByClassName('back-button')[0].classList.add('visible');
+              // fix for bad display lower than 1441 height
+              if(window.innerWidth < 1441) {
+                var newHeightDiv = document.getElementById('work').offsetHeight;
+                var workHeight = Math.floor(newHeightDiv/allWorks.length);
+                console.log(title.offsetHeight, text.offsetHeight, image.offsetHeight, workHeight)
+                var totalHeight = title.offsetHeight + text.offsetHeight + image.offsetHeight; // 600 for some margins
+                if(window.innerWidth < 980) {
+                  totalHeight += 600;
+                }
+                var indexOfLastVisibleDiv = allWorks.length-1;
+                console.log(totalHeight, newHeightDiv);
+                while(totalHeight < newHeightDiv) {
+                  console.log(totalHeight, newHeightDiv);
+                  allWorks[indexOfLastVisibleDiv].style.display = "none";
+                  indexOfLastVisibleDiv -= 1;
+                  newHeightDiv -= workHeight;
+                }
+                if(window.innerWidth < 980) {
+                  title.scrollIntoView({block: "start", inline: "start"});
+                }
+              }
             });
           }, 450);
         })
@@ -171,7 +277,7 @@ function main() {
   var backButton = document.getElementsByClassName('back-button')[0];
   backButton.onclick = () => {
     details.getElementsByClassName('work-details-inner')[0].getElementsByTagName('p')[0].classList.remove('visible');
-    details.getElementsByClassName('work-img')[0].classList.remove('visible');
+    image.classList.remove('visible');
     details.getElementsByClassName('back-button')[0].classList.remove('visible');
     title.innerHTML = "<span>|</span>";
     details.style.display = "none";
@@ -257,7 +363,7 @@ function createWorkGrid(allWorksArray) {
     var newText = document.createElement('p');
     var newTextDiv = document.createElement('div');
     var newTag = document.createElement('p');
-    newImage.src = allWorks[i][1];
+    newImage.src = allWorks[i][1][0];
     newElement.className = "single-work";
     newTextDiv.dataset.index = i;
     newText.dataset.index = i;
